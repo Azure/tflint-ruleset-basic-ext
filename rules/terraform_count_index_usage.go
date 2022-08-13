bf38e71cb6b28012794e95711dde49c5e91812dc
lunz0223 <lunz@uchicago.edu>
2022-08-14 00:41:04 +0800

    modify the implementation of terraform_count_index_usage rule

--- rules/terraform_count_index_usage.go
+++ rules/terraform_count_index_usage.go
@@ -6,8 +6,6 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 	"github.com/terraform-linters/tflint-ruleset-basic-ext/project"
-	"go/ast"
-	"reflect"
 )
 
 // TerraformCountIndexUsageRule checks whether count.index is used as subscript of list/map
@@ -82,81 +80,116 @@ func (r *TerraformCountIndexUsageRule) visitBlock(runner tflint.Runner, block *h
 }
 
 func (r *TerraformCountIndexUsageRule) visitExp(runner tflint.Runner, exp hclsyntax.Expression) error {
-	return r.diagnose(runner, reflect.ValueOf(exp), false)
-}
-
-func (r *TerraformCountIndexUsageRule) diagnose(runner tflint.Runner, x reflect.Value, asIndex bool) error {
-	if !ast.NotNilFilter("", x) {
-		return nil
+	file, _ := runner.GetFile(exp.Range().Filename)
+	tokens, diags := hclsyntax.LexExpression(exp.Range().SliceBytes(file.Bytes), exp.Range().Filename, exp.StartRange().Start)
+	if diags.HasErrors() {
+		return diags
 	}
-
 	var err error
-
-	switch x.Kind() {
-
-	case reflect.Interface:
-		err = r.diagnose(runner, x.Elem(), asIndex)
-
-	case reflect.Map:
-		if x.Len() > 0 {
-			for _, key := range x.MapKeys() {
-				if subErr := r.diagnose(runner, x.MapIndex(key), asIndex); subErr != nil {
-					err = multierror.Append(err, subErr)
-				}
-			}
-		}
-
-	case reflect.Pointer:
-		if x.Type().String() == "*hclsyntax.IndexExpr" {
-			asIndex = true
-		} else if asIndex && x.Type().String() == "*hclsyntax.ScopeTraversalExpr" {
-			traversal, isTraversal := x.Elem().FieldByName("Traversal").Interface().(hcl.Traversal)
-			if !isTraversal || len(traversal) < 2 {
-				return nil
-			}
-			root, isRoot := traversal[0].(hcl.TraverseRoot)
-			attr, isAttr := traversal[1].(hcl.TraverseAttr)
-			if isRoot && isAttr && root.Name == "count" && attr.Name == "index" {
-				runner.EmitIssue(
-					r,
-					"`count.index` is not recommended to be used as the subscript of list/map, use for_each instead",
-					traversal.SourceRange(),
-				)
-			}
-			return nil
-		}
-		err = r.diagnose(runner, x.Elem(), asIndex)
-
-	case reflect.Array:
-		if x.Len() > 0 {
-			for i, n := 0, x.Len(); i < n; i++ {
-				if subErr := r.diagnose(runner, x.Index(i), asIndex); subErr != nil {
-					err = multierror.Append(err, subErr)
-				}
-			}
-		}
-
-	case reflect.Slice:
-		if _, ok := x.Interface().([]byte); ok {
-			return nil
-		}
-		if x.Len() > 0 {
-			for i, n := 0, x.Len(); i < n; i++ {
-				if subErr := r.diagnose(runner, x.Index(i), asIndex); subErr != nil {
-					err = multierror.Append(err, subErr)
-				}
-			}
-		}
-
-	case reflect.Struct:
-		t := x.Type()
-		for i, n := 0, t.NumField(); i < n; i++ {
-			if name := t.Field(i).Name; ast.IsExported(name) {
-				if subErr := r.diagnose(runner, x.Field(i), asIndex); subErr != nil {
-					err = multierror.Append(err, subErr)
+	subscriptLevel := 0
+	for i, token := range tokens {
+		switch token.Type {
+		case hclsyntax.TokenOBrack:
+			subscriptLevel++
+		case hclsyntax.TokenCBrack:
+			subscriptLevel--
+		case hclsyntax.TokenIdent:
+			first := string(token.Bytes)
+			if subscriptLevel > 0 && first == "count" && i+2 < len(tokens) {
+				second := string(tokens[i+1].Bytes)
+				thrid := string(tokens[i+2].Bytes)
+				if second == "." && thrid == "index" {
+					subErr := runner.EmitIssue(
+						r,
+						"`count.index` is not recommended to be used as the subscript of list/map, use for_each instead",
+						hcl.Range{
+							Filename: token.Range.Filename,
+							Start:    token.Range.Start,
+							End:      tokens[i+2].Range.End,
+						},
+					)
+					if subErr != nil {
+						err = multierror.Append(err, subErr)
+					}
 				}
 			}
 		}
 	}
 	return err
 }
+
+//func (r *TerraformCountIndexUsageRule) diagnose(runner tflint.Runner, x reflect.Value, asIndex bool) error {
+//	if !ast.NotNilFilter("", x) {
+//		return nil
+//	}
+//
+//	var err error
+//
+//	switch x.Kind() {
+//
+//	case reflect.Interface:
+//		err = r.diagnose(runner, x.Elem(), asIndex)
+//
+//	case reflect.Map:
+//		if x.Len() > 0 {
+//			for _, key := range x.MapKeys() {
+//				if subErr := r.diagnose(runner, x.MapIndex(key), asIndex); subErr != nil {
+//					err = multierror.Append(err, subErr)
+//				}
+//			}
+//		}
+//
+//	case reflect.Pointer:
+//		if x.Type().String() == "*hclsyntax.IndexExpr" {
+//			asIndex = true
+//		} else if asIndex && x.Type().String() == "*hclsyntax.ScopeTraversalExpr" {
+//			traversal, isTraversal := x.Elem().FieldByName("Traversal").Interface().(hcl.Traversal)
+//			if !isTraversal || len(traversal) < 2 {
+//				return nil
+//			}
+//			root, isRoot := traversal[0].(hcl.TraverseRoot)
+//			attr, isAttr := traversal[1].(hcl.TraverseAttr)
+//			if isRoot && isAttr && root.Name == "count" && attr.Name == "index" {
+//				runner.EmitIssue(
+//					r,
+//					"`count.index` is not recommended to be used as the subscript of list/map, use for_each instead",
+//					traversal.SourceRange(),
+//				)
+//			}
+//			return nil
+//		}
+//		err = r.diagnose(runner, x.Elem(), asIndex)
+//
+//	case reflect.Array:
+//		if x.Len() > 0 {
+//			for i, n := 0, x.Len(); i < n; i++ {
+//				if subErr := r.diagnose(runner, x.Index(i), asIndex); subErr != nil {
+//					err = multierror.Append(err, subErr)
+//				}
+//			}
+//		}
+//
+//	case reflect.Slice:
+//		if _, ok := x.Interface().([]byte); ok {
+//			return nil
+//		}
+//		if x.Len() > 0 {
+//			for i, n := 0, x.Len(); i < n; i++ {
+//				if subErr := r.diagnose(runner, x.Index(i), asIndex); subErr != nil {
+//					err = multierror.Append(err, subErr)
+//				}
+//			}
+//		}
+//
+//	case reflect.Struct:
+//		t := x.Type()
+//		for i, n := 0, t.NumField(); i < n; i++ {
+//			if name := t.Field(i).Name; ast.IsExported(name) {
+//				if subErr := r.diagnose(runner, x.Field(i), asIndex); subErr != nil {
+//					err = multierror.Append(err, subErr)
+//				}
+//			}
+//		}
+//	}
+//	return err
+//}
