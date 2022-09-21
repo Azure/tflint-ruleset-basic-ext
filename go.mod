e95b46a883a43ad3829e3ff21fc5b0a4f3299aed
zjhe <hezijie@microsoft.com>
2022-09-21 22:34:04 +0800

    use GITHUB_TOKEN to avoid rate limit issue

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/stretchr/testify v1.7.2
 	github.com/terraform-linters/tflint-plugin-sdk v0.13.0
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
 
 require (
--- version/version_ensure_test.go
+++ version/version_ensure_test.go
@@ -3,18 +3,20 @@ package version_test
 import (
 	"context"
 	"fmt"
-	"github.com/terraform-linters/tflint-ruleset-basic-ext/project"
-	"golang.org/x/mod/semver"
+	"os"
 	"testing"
 
 	"github.com/google/go-github/v47/github"
 	"github.com/stretchr/testify/assert"
+	"github.com/terraform-linters/tflint-ruleset-basic-ext/project"
+	"golang.org/x/mod/semver"
+	"golang.org/x/oauth2"
 )
 
 func Test_EnsureVersionHasBeenBumpedUp(t *testing.T) {
 	currentVersion := fmt.Sprintf("v%s", project.Version)
 	assert.True(t, semver.IsValid(currentVersion))
-	client := github.NewClient(nil)
+	client := gitClient()
 	tags, _, err := client.Repositories.ListTags(context.TODO(), "Azure", "tflint-ruleset-basic-ext", &github.ListOptions{
 		Page:    0,
 		PerPage: 10,
@@ -27,4 +29,19 @@ func Test_EnsureVersionHasBeenBumpedUp(t *testing.T) {
 			t.Fatalf("latest version: %s, current version %s, please update current version in project/main.go", v, currentVersion)
 		}
 	}
+}
+
+func gitClient() *github.Client {
+	var client *github.Client
+	token := os.Getenv("TOKEN")
+	if token != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		tc := oauth2.NewClient(context.TODO(), ts)
+		client = github.NewClient(tc)
+	} else {
+		client = github.NewClient(nil)
+	}
+	return client
 }
