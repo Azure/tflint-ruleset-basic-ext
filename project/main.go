9ea731bea494ef41377334cc1767922e2ab6e0ad
zjhe <hezijie@microsoft.com>
2022-10-20 10:09:07 +0800

    fix incorrect reference link

--- project/main.go
+++ project/main.go
@@ -7,5 +7,5 @@ const Version string = "0.1.1"
 
 // ReferenceLink returns the rule reference link
 func ReferenceLink(name string) string {
-	return fmt.Sprintf("https://github.com/terraform-linters/tflint-ruleset-basic-ext/blob/%s/docs/rules/%s.md", Version, name)
+	return fmt.Sprintf("https://github.com/Azure/tflint-ruleset-basic-ext/blob/%s/docs/rules/%s.md", Version, name)
 }
