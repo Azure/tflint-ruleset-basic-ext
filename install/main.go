79972b0ed08e0ccacc622a1c69d9688e61479d86
zjhe <hezijie@microsoft.com>
2022-11-03 16:28:56 +0800

    add codeql and gosec to ci, fix install with TFLINT_PLUGIN_DIR env set

--- install/main.go
+++ install/main.go
@@ -13,9 +13,14 @@ func main() {
 		baseDir := os.Getenv("USERPROFILE")
 		outputDir = fmt.Sprintf(`%s\.tflint.d\plugins`, baseDir)
 	}
-	_ = os.MkdirAll(outputDir, os.ModePerm)
+	if dir := os.Getenv("TFLINT_PLUGIN_DIR"); dir != "" {
+		outputDir = dir
+	} else {
+		_ = os.MkdirAll(outputDir, os.ModePerm)
+	}
+	//#nosec G204
 	cmd := exec.Command("go", "build", "-o", outputDir)
 	if err := cmd.Run(); err != nil {
-		panic(err)
+		panic(err.Error())
 	}
 }
